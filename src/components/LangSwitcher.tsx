2325c0cb38d21b82326f60f3077093dcda676bd3
guyandjack <guillaume-d@tutanota.com>
2025-11-28 16:30:06 +0100

    details lang up date

--- src/components/LangSwitcher.tsx
+++ src/components/LangSwitcher.tsx
@@ -1,58 +1,72 @@
-//import des hook
+import { useEffect, useRef } from "preact/hooks";
 import { langSignal } from "../utils/langSignal";
-import { useTranslation } from "react-i18next";
-import { useRef } from "preact/hooks";
-
-//import des data
 import { languageRef } from "@/data/content/components/languageNumber/languageRef";
 
 const SelectLanguage = () => {
-  
-  const selectLang = (ref: string) => {
-    langSignal.value = ref;
+  const detailEl = useRef<HTMLDetailsElement | null>(null);
+
+  const closeDetail = () => {
+    const el = detailEl.current;
+    if (!el) return;
+    el.removeAttribute("open");
   };
 
+  const selectLang = (ref: string) => {
+    langSignal.value = ref;
+    closeDetail();
+  };
+
+  useEffect(() => {
+    const el = detailEl.current;
+    if (!el) return;
+
+    el.addEventListener("mouseleave", closeDetail);
+    return () => {
+      el.removeEventListener("mouseleave", closeDetail);
+    };
+  }, []);
+
   return (
-    <details className="dropdown group w-25 ">
+    <details ref={detailEl} className="dropdown group w-25">
       <summary
         className={[
           "btn m-1 flex flex-row justify-center gap-3",
-          //ajout de la tramsition
           "transition-all duration-300 ease-out",
           "hover:bg-primary/40",
         ].join(" ")}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802"
-          />
-        </svg>
+        {
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            {" "}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802"
+            />{" "}
+          </svg>
+        }
       </summary>
 
       <ul
         className={[
           "menu dropdown-content bg-base-100 rounded-box z-10 w-25 p-2 shadow-sm",
-          // 🪄 Ajout de la transition personnalisée
           "transition-all duration-300 ease-out origin-top overflow-hidden",
           "max-h-0 opacity-0 scale-95",
           "group-open:max-h-96 group-open:opacity-100 group-open:scale-100",
         ].join(" ")}
       >
-        {languageRef.map(([code, label],index) => (
+        {languageRef.map(([code, label], index) => (
           <li className="flex-row justify-center" key={code + index.toString()}>
             <button
               className={[
                 "p-2 w-15 flex-row justify-center items-center",
-                //ajout de la tramsition
                 "transition-all duration-300 ease-out",
                 "hover:text-primary",
               ].join(" ")}
--- src/data/content/components/languageNumber/languageRef.ts
+++ src/data/content/components/languageNumber/languageRef.ts
@@ -5,8 +5,8 @@ type language = [string, string][]
 const languageRef:language = [
     ["fr", "Français"],
     ["en","English"],
-    ["en","Deutch"],
-    ["en","italiano"]
+   /*  ["en","Deutch"],
+    ["en","italiano"] */
 ]
 
 export {languageRef}
